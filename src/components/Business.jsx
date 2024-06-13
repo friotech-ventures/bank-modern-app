7fd367171c3184beb957eacc22f22fb51891f4c9
friotech ventures <108271300+friotech-ventures@users.noreply.github.com>
2024-06-13 23:38:57 +0530

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,20 @@ import { features } from "../constants";
 import styles, { layout } from "../styles";
 import Button from "./Button";
 
+<section id="product" className={layout.sectionReverse}>
+    {/* Left Side */}
+    <div className={layout.sectionImgReverse}>
+      {/* Background Image */}
+      <img
+        src={bill}
+        alt="Billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+    </div>
+
+
 // Feature Card
 const FeatureCard = ({ icon, title, content, index }) => (
   <div
